49902a8fdcb16fd23183371f77ed223347f641f5
meet <meetpadaliya55@gmail.com>
2025-04-30 20:59:05 +0530

    index on feature/home-page: 6c8a625 add stats section and changes into header,services,whyChooseUs and some css

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -13,6 +13,9 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Jadeja Pragati",
   description: "Recycling & Compliance",
+  icons: {
+    icon: "/icon.webp", 
+  },
 };
 
 export default function RootLayout({
